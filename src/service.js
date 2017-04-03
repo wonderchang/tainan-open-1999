6aca8b7f2f76c7f6ca1bbfce4887eddfe76d61f3
Wonder Chang <wonderchang@kkbox.com>
2017-04-03 22:45:29 +0800

    Wrap service resource

--- src/service.js
+++ src/service.js
@@ -1,8 +1,21 @@
 const request = require('request')
+const lodash = require('lodash')
 const json2xml = require('jsontoxml')
 const xmlJsonParser = require('xml2json')
 const utils = require('./utils')
 const config = require('./config')
+
+const wrapServiceResource = (o) => ({
+  code: o.service_code,
+  name: o.service_name,
+  subjects: o.subproject.split(',').filter(d => '' !== d).map(d => {
+    const [code, name] = d.split(' ')
+    return {
+      code: code,
+      name: name,
+    }
+  })
+})
 
 const getList = (callback) => {
   const body = json2xml(utils.wrapValueCdataTag({city_id: config.CITY_ID}))
@@ -17,7 +30,7 @@ const getList = (callback) => {
       }
       callback(null, {
         num: parseInt(data.root.count),
-        services: data.root.records.record,
+        services: data.root.records.record.map(wrapServiceResource),
       })
     })
   })
